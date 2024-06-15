d71b643cc0601328a8b0a4ce99f650382891eb2f
Vasyahhh <vasyahchain16@gmail.com>
2024-06-15 23:15:08 +0700

    update docs

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,15 @@ const authRoute = require('../route/auth-route');
 const bookmarkRoute = require('../route/bookmark-route.js');
 const statusRoute = require('../route/status-route.js');
 const plantRoute = require('../route/plant-route.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../config/swagger.json');
 const app = express()
 
 
 app.use(cors())
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/auth', authRoute);
 app.use('/bookmark', bookmarkRoute);
